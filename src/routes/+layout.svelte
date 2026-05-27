cc515814f9c91df4bf291e79b62cf044d635b1df
erxclau <ericlau41802@gmail.com>
2026-05-27 18:23:32 -0400

    Fix favicon asset

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
+	import { asset } from '$app/paths';
+
 	let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="icon" href="/favicon.ico" />
+	<link rel="icon" href={asset('/favicon.ico')} />
 </svelte:head>
 
 {@render children()}
